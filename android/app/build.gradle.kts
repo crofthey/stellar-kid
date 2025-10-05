aeda24b2f79f446423e0e54315ffe76a43397f44
Matt Clegg <mattjclegg@gmail.com>
2025-10-05 15:58:33 +0100

    Initial commit with bootable android app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,6 +43,11 @@ android {
         }
     }
 
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -103,8 +108,9 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.11.0")
 
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     implementation("com.google.dagger:hilt-android:2.52")
--- android/core/network/src/main/kotlin/com/stellarkid/core/network/NetworkConfig.kt
+++ android/core/network/src/main/kotlin/com/stellarkid/core/network/NetworkConfig.kt
@@ -4,10 +4,12 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import kotlinx.serialization.ExperimentalSerializationApi
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 
+@OptIn(ExperimentalSerializationApi::class)
 object NetworkConfig {
     private val json = Json {
         ignoreUnknownKeys = true
