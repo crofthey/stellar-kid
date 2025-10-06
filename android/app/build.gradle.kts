a2cc3e4415e37a2b74cb6c5d276b0d7c5933a1b3
Matt <matt@example.com>
2025-10-06 16:40:47 +0100

    more fixes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -97,6 +97,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.6")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
